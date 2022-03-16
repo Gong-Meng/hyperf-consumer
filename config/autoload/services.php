3ad97a0b2e9208d6a34ebd217ae25d5bbb642edd
gongmeng <18588264128@163.com>
2022-03-16 11:04:51 +0800

    [服务消费者] 处理

--- config/autoload/services.php
+++ config/autoload/services.php
@@ -12,19 +12,12 @@ declare(strict_types=1);
 return [
     'consumers' => [
         [
-            // The service name, this name should as same as with the name of service provider.
             'name' => 'CalculatorService',
-            // The service registry, if `nodes` is missing below, then you should provide this configs.
             'service' => \App\JsonRpc\CalculatorServiceInterface::class,
             'registry' => [
                 'protocol' => 'consul',
                 'address' => 'http://127.0.0.1:8500',
             ],
-            // If `registry` is missing, then you should provide the nodes configs.
-//            'nodes' => [
-//                // Provide the host and port of the service provider.
-//                ['host' => '127.0.0.1', 'port' => 9503]
-//            ],
         ],
     ],
 ];
